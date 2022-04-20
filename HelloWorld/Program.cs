8c05490f27d5d7c866052bf792d334a40f01a9be
Cooper Kozitza <cooperkozitza@gmail.com>
2022-04-19 21:59:17 -0700

    Added PaginationObject Class, Added Box Art Images

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -21,7 +21,7 @@ namespace HelloWorld
         }
 
         public static TwitchAPI api = new TwitchAPI("or94frz4dm83ru11ql2x9jlik2x9hd", "bscma5xlv1ylbfxj8wafj1jmkt2ycs");
-        public static List<Game> games = JsonConvert.DeserializeObject<List<Game>>(api.GetTopTenJSON());
+        public static IList<Game> games = api.GetTopTenJSON();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
@@ -39,31 +39,35 @@ namespace HelloWorld
             Id = id;
         }
 
-
         protected string Auth { get; set; }
         protected string Id { get; set; }
 
         protected WebClient client = new WebClient();
-        public class TwitchApiReturn
+        public class TwitchApiOutput
         {
-            public string data { get; set; }
-            public string pagination { get; set; }
+            public Game[] Data { get; set; }
+            public PaginationObject Pagination { get; set; }
         }
 
-        public string GetTopTenJSON()
+        public class PaginationObject
+        {
+            public string cursor { get; set; }
+        }
+
+        public IList<Game> GetTopTenJSON()
         {
             client.Headers.Add("Authorization: Bearer " + Auth);
             client.Headers.Add("Client-Id: " + Id);
-            var api = client.DownloadString("https://api.twitch.tv/helix/games/top");
-            TwitchApiReturn twitchApiReturn = JsonConvert.DeserializeObject<TwitchApiReturn>(api);
-            return twitchApiReturn.data;
+            var apiJson = client.DownloadString("https://api.twitch.tv/helix/games/top");
+            TwitchApiOutput twitchApiOutput = JsonConvert.DeserializeObject<TwitchApiOutput>(apiJson);
+            return twitchApiOutput.Data.ToList();
         }
     }
 
     public class Game
     {
-        public string id { get; set; }
-        public string name { get; set; }
-        public string box_art_url { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Box_art_url { get; set; }
     }
 }
